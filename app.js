3a15aa1062d6f41ade31b508c48b385b3f643430
Stefanus Tobing <stefanustobing@gmail.com>
2023-11-14 20:58:34 +0100

    Add swagger documentation UI

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors=require('cors');
 const xss=require('xss-clean');
 const rateLimiter= require('express-rate-limit');
 
+//Swagger
+const swaggerUI=require('swagger-ui-express');
+const YAML=require('yamljs');
+const swaggerDocument=YAML.load('./swagger.yaml')
+
 
 const express = require('express');
 const app = express();
@@ -41,8 +46,9 @@ app.use(xss());
 
 // routes
 app.get('/',(req,res)=>{
-  res.send('jobs api');
+  res.send('<h1>jobs API</h1><a href="/api-docs">Documentation</a>');
 })
+app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/jobs', authenticationMiddleware,jobRouter);
 
